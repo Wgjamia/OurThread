6ad813f5d96f4d45afc9592b17c4bb6467bbb188
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-09-21 20:27:53 +0200

    Changes have been committed.

---
+++ src/pg/thread/MyThread.java
@@ -0,0 +1,15 @@
+package pg.thread;
+
+public class MyThread extends Thread {
+
+	public void run() {
+		System.out.println("concurrent thread started running..");
+	}
+
+	public static void main(String[] args) {
+		MyThread myThread = new MyThread();
+		myThread.start();
+
+	}
+
+}
